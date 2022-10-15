de0a332a9b3623b18111ead10f5ac455d8afbbfc
Sahar11 <saharhalim151@gmail.com>
2022-10-15 16:42:49 -0400

    add speechly polyfill

--- front-end/src/entry/Entry.jsx
+++ front-end/src/entry/Entry.jsx
@@ -2,6 +2,12 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import './entry.css';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
+import axios from 'axios';
+
+const appId = 'c9822203-dbe0-4fe1-bcfa-d0e9f73314d9';
+const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
+SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
 
 export default function Entry () {
 
@@ -15,7 +21,7 @@ export default function Entry () {
 
   const Submit = () => {
     useEffect(() => {
-      //axios
+      axios.post(`localhost:9000/entry/${transcript}`)
     })
   }
 
@@ -46,9 +52,14 @@ export default function Entry () {
     <div className="entry-block">
       <div>
       <p>Listening: {listening ? 'on' : 'off'}</p>
-      <button id="button" onClick={listenContinously}>Start</button>
+      {/* <button id="button" onClick={listenContinously}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
+      <button onClick={resetTranscript}>Reset</button> */}
+      <button onTouchStart={listenContinously}
+        onMouseDown={listenContinously}
+        onTouchEnd={SpeechRecognition.stopListening}
+        onMouseUp={SpeechRecognition.stopListening} 
+        > Hold to talk </button>
       </div>
       <div className="entry-form">
       <form onSubmit={Submit}>
