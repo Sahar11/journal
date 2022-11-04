3795902f90dc018b3b5410639b6e8de9afd8504b
Sahar11 <saharhalim151@gmail.com>
2022-11-04 17:36:54 -0400

    add dotenv

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,14 @@
+// db connection
+const connectionString = process.env.CONNECTION_STRING
+const pgp = require("pg-promise")();
+const db = pgp(connectionString);
 const express = require("express");
 const PORT = 9000;
 const app =express();
+
+app.post('/', (res,req) => {
+  
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT} :)`)
