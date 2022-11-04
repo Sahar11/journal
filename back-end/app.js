11fabc21f75c4657e37145444256794108582322
Sahar11 <saharhalim151@gmail.com>
2022-11-04 19:28:09 -0400

    add entry route

--- back-end/app.js
+++ back-end/app.js
@@ -8,9 +8,27 @@ const { ENVIROMENT, PORT } = process.env;
 const express = require("express");
 const app =express();
 
-app.post('/', (res,req) => {
-  
+app.post('/entry', async (res,req) => {
+
+  const {
+   entry_text
+  } = req.body;
+  try {
+
+    //
+     const results = await db.query(`INSERT INTO ENTRY (person_id, entry_text) VAUES (1, $1 )  RETURNING *`, [entry_text]);
+     res.json(results);
+   } catch (err) {
+     console.log(err);
+   }
 });
+/*
+  person_id int,
+  entry_id SERIAL PRIMARY KEY,
+  entry_date DATE,
+  entry_time TIME,
+  entry_text VARCHAR(2000) ,
+*/
 
 app.listen(8000, () => {
   console.log(`App is listening 🙂 🙂 `)
